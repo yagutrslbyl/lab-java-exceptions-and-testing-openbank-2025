dfa1fef0f05e3efd52ed97b24500dcc663525ebd
yagutrslbyl <yaguter-bpa202@code.edu.az>
2026-03-17 00:54:42 +0400

    Fixedannotation

--- demo/src/test/java/com/example/demo/ProductControllerTest.java
+++ demo/src/test/java/com/example/demo/ProductControllerTest.java
@@ -6,6 +6,7 @@ import com.example.demo.services.ProductService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -28,7 +29,7 @@ class ProductControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockitoBean
+    @MockBean
     private ProductService productService;
 
     @Test
